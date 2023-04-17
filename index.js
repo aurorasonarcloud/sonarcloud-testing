0fc1d7e922e5fd218368d0e91176d48962714cd0
Tim_Jones <tjones@aurorasolar.com>
2023-04-17 09:38:42 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import { addNumbers, subtractNumbers, multiplyNumbers } from './numbers.js';
+
+console.log('Adding some stuff: ', addNumbers(1, 2));
+
+console.log('Subtracting some stuff: ', subtractNumbers(1, 2));
+
+console.log('Multiplying some stuff: ', multiplyNumbers(1, 2));
+
+console.log('Joining some strings: ', joinStrings('Hello', 'World'));
---
+++ numbers.js
@@ -0,0 +1,15 @@
+export function addNumbers(a, b) {
+  return a + b;
+}
+
+export function subtractNumbers(a, b) {
+  return a - b;
+}
+
+export function multiplyNumbers(a, b) {
+  return a * b;
+}
+
+export function divideNumbers(a, b) {
+  return a / b;
+}
